6c5467f7280bec3cf6d0722535fd59ab4cfe7774
baodongle <baodongle@gmail.com>
2019-07-24 13:45:33 +0700

    Init app

---
+++ imdbtop10/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'prettier/prettier': ['error', { 'trailingComma': 'es5', 'singleQuote': true, 'printWidth': 100 }],
+    'react/prefer-stateless-function': [0],
+  },
+  plugins: [
+    'prettier',
+    'react',
+    'react-native',
+  ],
+};
